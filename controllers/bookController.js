589136544d0d932354ec4cd5008470482cc4e42d
Maxx <maxhiga.hawaii@gmail.com>
2021-03-21 17:11:30 -1000

    started backend frame

---
+++ controllers/bookController.js
@@ -0,0 +1,33 @@
+const Book = require("../models/bookModel");
+const axios = require("axios");
+
+module.exports = {
+  //getting all saved books from db
+  getSaved: async (req, res) => {
+    console.log("got books");
+
+    try {
+      const allBooks = await Book.find();
+      res.json(allBooks);
+    } catch (err) {
+      console.log("get books err", err);
+      res.send("couldn't get books", err);
+    }
+  },
+
+  searchBooks: async (req, res) => {
+    console.log("searched books");
+    const searchParam = req.body.searchInput;
+    const KEY = process.env.API_KEY;
+    try {
+      const searching = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchParam}&key=${KEY}`
+      );
+      res.json(searching);
+      console.log("searching ", searching);
+    } catch (err) {
+      console.log("get search err", err);
+      res.send("couldn't search books", err);
+    }
+  },
+};
